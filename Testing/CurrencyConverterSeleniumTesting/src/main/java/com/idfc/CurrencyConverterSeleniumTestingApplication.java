b1fb2878b011f82a7a22d6606d58cbce97d0befb
AkashNadar <Akashnadar14@gmail.com>
2022-10-20 15:33:25 +0530

    Testing done using Selenum

---
+++ Testing/CurrencyConverterSeleniumTesting/src/main/java/com/idfc/CurrencyConverterSeleniumTestingApplication.java
@@ -0,0 +1,13 @@
+package com.idfc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConverterSeleniumTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConverterSeleniumTestingApplication.class, args);
+	}
+
+}
